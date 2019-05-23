a465d5acab1267041959c3fc8e878653e805c0a5
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-05-23 11:53:26 +0200

    Add sample react-native components (header and footer).

--- WorldDemo/App.js
+++ WorldDemo/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Switch, Text, View } from 'react-native';
 import { Scene3dView } from 'react-native-scene3d';
 
 export default class App extends React.Component {
+  state = { 
+    debugNodesValue: false,
+  }
+
+  onSwitchValueChange = () => {
+    const value = this.state.debugNodesValue ? false : true;
+    this.setState({ debugNodesValue: value });
+  }
+
   render() {
     return (
-      <Scene3dView showStatistics={true} style={styles.container}>
-        <Text>Sample</Text>
-      </Scene3dView>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>Sample React Native App</Text>
+        <Scene3dView showStatistics={true} style={styles.scene3d}>
+          <Text>Sample</Text>
+        </Scene3dView>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Render debug nodes</Text>
+          <Switch
+            value={this.state.debugNodesValue}
+            style={styles.switch}
+            onValueChange={this.onSwitchValueChange}
+          />
+        </View>
+        
+      </View>
     );
   }
 }
@@ -17,6 +38,40 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  titleText: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: '900',
+    fontStyle: 'normal',
+    fontFamily: 'System',
+    lineHeight: 41,
+    marginTop: 44,
+  },
+  scene3d: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#555555',
   },
+  footer: {
+    width: '90%',
+    height: 60,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    marginBottom: 30,
+  },
+  footerText: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: '400',
+    fontStyle: 'normal',
+    fontFamily: 'System',
+  },
+  switch: {
+    marginLeft: 10,
+  }
 });

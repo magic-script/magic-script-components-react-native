53960afa1c6f0415dc0df728ce55a8e9d133a7fd
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-05-29 21:44:37 +0200

    Clean project folder.

--- WorldDemo/ios/EmptyFile.swift
+++
@@ -1,9 +0,0 @@
-//
-//  EmptyFile.swift
-//  WorldDemo
-//
-//  Created by Pawel Leszkiewicz on 23/05/2019.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
-
-import Foundation
--- scene3d/index.js
+++
@@ -1,7 +0,0 @@
-import Scene3dView from './Scene3dView';
-
-export { Scene3dView };
-
-// import { NativeModules } from 'react-native';
-// const { RNScene3d } = NativeModules;
-// export default RNScene3d;

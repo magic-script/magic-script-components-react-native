ce283877533794aed101cc122297b68f644d36be
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-07-08 13:40:39 +0200

    Fix typo.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import ARView from './ARView';
 import ReactNativeMagicScript from './components/react-native-magic-script/react-native-magic-script';
 
 // backward compatibility, should be removed in the future
-const ARKitScript = ReactNativeMagicScript,
+const ARKitScript = ReactNativeMagicScript;
 
 export {
   ARView,

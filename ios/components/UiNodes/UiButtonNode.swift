31ec6107398f06e2028f2c2eeb1e4b8bbeb4edad
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-08-09 16:24:24 +0200

    Increase side insets for UiButton component.

--- ios/components/UiNodes/UiButtonNode.swift
+++ ios/components/UiNodes/UiButtonNode.swift
@@ -132,7 +132,7 @@ import SceneKit
 
     @objc override func _calculateSize() -> CGSize {
         let labelSize = labelNode.getSize()
-        let contentWidth: CGFloat = (width > 0) ? width : labelSize.width + 2 * UiButtonNode.borderInset
+        let contentWidth: CGFloat = (width > 0) ? width : labelSize.width + 2 * UiButtonNode.borderInset + labelSize.height 
         let contentHeight: CGFloat = (height > 0) ? height : labelSize.height + 2 * UiButtonNode.borderInset
         return CGSize(width: contentWidth, height: contentHeight)
     }

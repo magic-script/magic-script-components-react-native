4db139c63b8466e2fd0af5117844406dbcd1f76a
Dariusz Konieczko <d.konieczko@nomtek.com>
2019-08-13 11:02:09 +0200

    code cleaning and refactoring

--- android/src/main/java/com/reactlibrary/scene/nodes/layouts/LayoutManager.kt
+++ android/src/main/java/com/reactlibrary/scene/nodes/layouts/LayoutManager.kt
@@ -4,7 +4,8 @@ import com.google.ar.sceneform.Node
 import com.reactlibrary.scene.nodes.props.Bounding
 
 /**
- * Grid manager that places the children nodes in the correct positions
+ * Layout manager is responsible for placing the children nodes in
+ * the correct positions inside [UiLayout]
  */
 interface LayoutManager {
     fun layoutChildren(children: List<Node>, childrenBounds: Map<Int, Bounding>)

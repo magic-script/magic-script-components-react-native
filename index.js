471a29490b2f732deb0247fa6def94d484fdd6b3
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-08-20 10:48:34 +0200

    #53 Expose components with uppercase names.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//
-import { NativeModules } from "react-native";
 import ARView from "./ARView";
 import ReactNativeMagicScript from "./components/react-native-magic-script/react-native-magic-script";
 
@@ -7,3 +5,42 @@ import ReactNativeMagicScript from "./components/react-native-magic-script/react
 const ARKitScript = ReactNativeMagicScript;
 
 export { ARView, ARKitScript, ReactNativeMagicScript };
+
+export {
+    // Audio,
+    Button,
+    // CircleConfirmation,
+    // ColorPicker,
+    // Content,
+    // DatePicker,
+    // Dialog,
+    // DropdownList,
+    // DropdownListItem,
+    GridLayout,
+    Image,
+    // Light,
+    Line,
+    // LinearLayout,
+    // ListView,
+    // ListViewItem,
+    Model,
+    // PageView,
+    // Panel,
+    // PortalIcon,
+    ProgressBar,
+    // Quad,
+    // RectLayout,
+    // ScrollBar,
+    // ScrollView,
+    // Slider,
+    Spinner,
+    // Tab,
+    Text,
+    // TextEdit,
+    // TimePicker,
+    Toggle,
+    // ToggleGroup,
+    Video,
+    View,
+    // WebView
+} from './components.js'

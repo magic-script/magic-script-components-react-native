fdd465a3c26915228e09a0c6dc83a3ef8862eb2f
Tim Caswell <tcaswell@magicleap.com>
2019-08-26 11:15:19 -0500

    Update postinstall.sh
    Use git clone urls for now while these are private repos.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Clone componencts repository for iOS
-git clone https://github.com/magic-script/magic-script-components-ios.git ios -b develop
+git clone git@github.com:magic-script/magic-script-components-ios.git ios -b develop
 # Clone componencts repository for Android
-git clone https://github.com/magic-script/magic-script-components-android.git android -b develop
+git clone git@github.com:magic-script/magic-script-components-android.git android -b develop

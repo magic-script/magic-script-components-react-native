5ad34453fe029d9e4f8b384a927a2fae5ffa72b1
Dariusz Konieczko <d.konieczko@nomtek.com>
2019-08-28 14:20:04 +0200

    removed the initAR method - not required anymore on Android

--- ARView.js
+++ ARView.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { requireNativeComponent, NativeModules } from 'react-native';
+import { requireNativeComponent } from 'react-native';
 
 const RCTARView = requireNativeComponent('RCTARView');
 
 export default class ARView extends React.Component {
-
-    constructor(props) {
-        super(props);
-        NativeModules.ARComponentManager.initAR();
-    }
-
     render() {
         return <RCTARView {...this.props} />
     }

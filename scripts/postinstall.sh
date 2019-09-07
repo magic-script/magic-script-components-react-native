1d8022d3f0c5b0dcd2a03aaa54336c72be2d4b24
unknown <mateusz.wojcik@sofomo.com>
2019-09-07 15:17:09 +0200

    branch rename

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,4 +2,4 @@
 # Clone components repository for iOS
 git clone git@github.com:magic-script/magic-script-components-ios.git ios -b develop
 # Clone components repository for Android
-git clone git@github.com:magic-script/magic-script-components-android.git -b feature/91 android
+git clone git@github.com:magic-script/magic-script-components-android.git -b feature/91_list_view android

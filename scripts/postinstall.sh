0c391ab834e93d234e3167edb14730d2145aa07b
unknown <mateusz.wojcik@sofomo.com>
2019-09-09 18:19:59 +0200

    import only android

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Clone components repository for iOS
-git clone git@github.com:magic-script/magic-script-components-ios.git ios -b develop
+# git clone git@github.com:magic-script/magic-script-components-ios.git ios -b develop
 # Clone components repository for Android
 git clone git@github.com:magic-script/magic-script-components-android.git -b feature/91_list_view android

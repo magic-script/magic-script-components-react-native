1e31ff57a1ae6ae3792670ecb586b0952c8564e7
panwrona <mariusz.brona@gmail.com>
2019-09-18 17:03:46 +0200

    remove postinstall script from repository

--- scripts/postinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Clone components repository for iOS
-git clone git@github.com:magic-script/magic-script-components-ios.git ios -b develop
-# Clone components repository for Android
-git clone git@github.com:magic-script/magic-script-components-android.git android -b develop

7afabb5fce9a94b86c1eeb46c0cbbe194495ff5d
Sebastian Paluchiewicz <sebastian.paluchiewicz@sofomo.com>
2019-10-23 12:38:07 +0200

    Uppercase event field name.

--- ios/RNMagicScript/bridge/AREventsManager.m
+++ ios/RNMagicScript/bridge/AREventsManager.m
@@ -126,7 +126,7 @@ RCT_EXPORT_MODULE();
 - (void)onSliderChangedEventReceived:(UiSliderNode *)sender value:(CGFloat)value {
     if (hasListeners) {
         dispatch_async(dispatch_get_main_queue(), ^{
-            [self sendEventWithName:@"onSliderChanged" body:@{ @"nodeId": sender.name, @"value": @(value) }];
+            [self sendEventWithName:@"onSliderChanged" body:@{ @"nodeId": sender.name, @"Value": @(value) }];
         });
     }
 }

c02dcf1c66b24bd94cef5c14c53bf08a24312a35
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-10-25 10:59:43 +0200

    Add localPosition in bounds calculation for toggle.

---
+++ ios/RNMagicScript/components/Utils/Extensions/CGPoint+Extension.swift
@@ -0,0 +1,77 @@
+//
+//  Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// 
+
+import CoreGraphics
+
+// MARK: - Addition
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func += ( left: inout CGPoint, right: CGPoint) {
+    left = left + right
+}
+
+// MARK: - Subtraction
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func -= (left: inout CGPoint, right: CGPoint) {
+    left = left - right
+}
+
+// MARK: - Division
+func / (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x / right.x, y: left.y / right.y)
+}
+
+func /= (left: inout CGPoint, right: CGPoint) {
+    left = left / right
+}
+
+func / (left: CGPoint, right: CGFloat) -> CGPoint {
+    return CGPoint(x: left.x / right, y: left.y / right)
+}
+
+func / (left: CGFloat, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left / right.x, y: left / right.y)
+}
+
+func /= (left: inout CGPoint, right: CGFloat) {
+    left = left / right
+}
+
+// MARK: - Multiplication
+func * (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x * right.x, y: left.y * right.y)
+}
+
+func *= (left: inout CGPoint, right: CGPoint) {
+    left = left * right
+}
+
+func * (left: CGPoint, right: CGFloat) -> CGPoint {
+    return CGPoint(x: left.x * right, y: left.y * right)
+}
+
+func * (left: CGFloat, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left * right.x, y: left * right.y)
+}
+
+func *= (left: inout CGPoint, right: CGFloat) {
+    left = left * right
+}

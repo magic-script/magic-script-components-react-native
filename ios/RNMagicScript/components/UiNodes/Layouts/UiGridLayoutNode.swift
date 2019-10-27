81ef340b15a3a079b7e64c914bf154344c77e3b8
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-10-27 19:47:11 +0100

    #184 Extract grid layout to separate module. Reuse it in rect, linear and grid layouts.

---
+++ ios/RNMagicScript/components/UiNodes/Layouts/UiGridLayoutNode.swift
@@ -0,0 +1,98 @@
+//
+//  Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import SceneKit
+
+@objc open class UiGridLayoutNode: UiNode {
+
+    @objc var columns: Int {
+        get { return gridLayout.columns }
+        set { gridLayout.columns = newValue; setNeedsLayout() }
+    }
+    @objc var rows: Int {
+        get { return gridLayout.rows }
+        set { gridLayout.rows = newValue; setNeedsLayout() }
+    }
+    @objc var defaultItemAlignment: Alignment {
+        get { return gridLayout.defaultItemAlignment }
+        set { gridLayout.defaultItemAlignment = newValue; setNeedsLayout() }
+    }
+    @objc var defaultItemPadding: UIEdgeInsets {
+        get { return gridLayout.defaultItemPadding }
+        set { gridLayout.defaultItemPadding = newValue; setNeedsLayout() }
+    }
+    @objc var skipInvisibleItems: Bool {
+        get { return gridLayout.skipInvisibleItems }
+        set { gridLayout.skipInvisibleItems = newValue; setNeedsLayout() }
+    }
+
+    fileprivate var gridLayout = GridLayout()
+
+    @objc override func setupNode() {
+        super.setupNode()
+        contentNode.addChildNode(gridLayout.container)
+    }
+
+    @objc override func hitTest(ray: Ray) -> TransformNode? {
+        return gridLayout.hitTest(ray: ray, node: self)
+    }
+
+    @objc override func update(_ props: [String: Any]) {
+        super.update(props)
+
+        if let columns = Convert.toInt(props["columns"]) {
+            self.columns = columns
+        }
+
+        if let rows = Convert.toInt(props["rows"]) {
+            self.rows = rows
+        }
+
+        if let defaultItemAlignment = Convert.toAlignment(props["defaultItemAlignment"]) {
+            self.defaultItemAlignment = defaultItemAlignment
+        }
+
+        if let defaultItemPadding = Convert.toPadding(props["defaultItemPadding"]) {
+            self.defaultItemPadding = defaultItemPadding
+        }
+
+        if let skipInvisibleItems = Convert.toBool(props["skipInvisibleItems"]) {
+            self.skipInvisibleItems = skipInvisibleItems
+        }
+    }
+
+    @objc override func addChild(_ child: TransformNode) {
+        gridLayout.addItem(child)
+        setNeedsLayout()
+    }
+
+    @objc override func removeChild(_ child: TransformNode) {
+        if gridLayout.removeItem(child) {
+            setNeedsLayout()
+        }
+    }
+
+    @objc override func _calculateSize() -> CGSize {
+        return gridLayout.getSize()
+    }
+
+    @objc override func updateLayout() {
+        // Invoke getSize to make sure the grid's sizes are calcualted and cached in gridDesc.
+        let _ = getSize()
+        gridLayout.updateLayout()
+    }
+}
---
+++ ios/RNMagicScript/components/UiNodes/Layouts/UiLinearLayoutNode.swift
@@ -0,0 +1,95 @@
+//
+//  Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// 
+
+import Foundation
+import SceneKit
+
+@objc open class UiLinearLayoutNode: UiLayoutNode {
+    @objc var layoutOrientation: Orientation = Orientation.vertical {
+        didSet {
+            switch layoutOrientation {
+            case .vertical:
+                gridLayout.columns = 1
+                gridLayout.rows = 0
+            case .horizontal:
+                gridLayout.columns = 0
+                gridLayout.rows = 1
+            }
+            setNeedsLayout()
+        }
+    }
+    @objc var defaultItemAlignment: Alignment = Alignment.centerCenter {
+        didSet { setNeedsLayout() }
+    }
+    @objc var defaultItemPadding: UIEdgeInsets = UIEdgeInsets.zero {
+        didSet { setNeedsLayout() }
+    }
+    @objc var skipInvisibleItems: Bool = false {
+        didSet { setNeedsLayout() }
+    }
+
+    fileprivate var gridLayout = GridLayout()
+
+    @objc override func setupNode() {
+        super.setupNode()
+        contentNode.addChildNode(gridLayout.container)
+    }
+
+    @objc override func hitTest(ray: Ray) -> TransformNode? {
+        return gridLayout.hitTest(ray: ray, node: self)
+    }
+
+    @objc override func update(_ props: [String: Any]) {
+        super.update(props)
+
+        if let orientation = Convert.toOrientation(props["orientation"]) {
+            self.layoutOrientation = orientation
+        }
+
+        if let defaultItemAlignment = Convert.toAlignment(props["defaultItemAlignment"]) {
+            self.defaultItemAlignment = defaultItemAlignment
+        }
+
+        if let defaultItemPadding = Convert.toPadding(props["defaultItemPadding"]) {
+            self.defaultItemPadding = defaultItemPadding
+        }
+
+        if let skipInvisibleItems = Convert.toBool(props["skipInvisibleItems"]) {
+            self.skipInvisibleItems = skipInvisibleItems
+        }
+    }
+
+    @objc override func addChild(_ child: TransformNode) {
+        gridLayout.addItem(child)
+        setNeedsLayout()
+    }
+
+    @objc override func removeChild(_ child: TransformNode) {
+        if gridLayout.removeItem(child) {
+            setNeedsLayout()
+        }
+    }
+
+    @objc override func _calculateSize() -> CGSize {
+        return gridLayout.getSize()
+    }
+
+    @objc override func updateLayout() {
+        // Invoke getSize to make sure the grid's sizes are calcualted and cached in gridDesc.
+        let _ = getSize()
+        gridLayout.updateLayout()
+    }
+}

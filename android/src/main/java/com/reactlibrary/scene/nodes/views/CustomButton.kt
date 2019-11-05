724520ac934d53d079a3ac15be4e89ba53ab1f92
Dariusz Konieczko <d.konieczko@nomtek.com>
2019-11-05 15:11:37 +0100

    fixes after code review

--- android/src/main/java/com/reactlibrary/scene/nodes/views/CustomButton.kt
+++ android/src/main/java/com/reactlibrary/scene/nodes/views/CustomButton.kt
@@ -263,15 +263,11 @@ class CustomButton @JvmOverloads constructor(
                 return drawable.bitmap
             }
         }
-        val bitmap = if (drawable.intrinsicWidth <= 0 || drawable.intrinsicHeight <= 0) {
-            Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
-        } else {
-            Bitmap.createBitmap(
-                    drawable.intrinsicWidth,
-                    drawable.intrinsicHeight,
-                    Bitmap.Config.ARGB_8888
-            )
-        }
+        val bitmap = Bitmap.createBitmap(
+                max(drawable.intrinsicWidth, 1),
+                max(drawable.intrinsicHeight, 1),
+                Bitmap.Config.ARGB_8888
+        )
         val canvas = Canvas(bitmap)
         drawable.setBounds(0, 0, canvas.width, canvas.height)
         drawable.draw(canvas)

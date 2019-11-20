53ae29db10d20ef72723aff7f19540463b27ad04
panwrona <mariusz.brona@gmail.com>
2019-11-20 12:09:50 +0100

    Improvements after code review

--- android/src/main/java/com/reactlibrary/scene/nodes/views/MaxHeightScrollView.kt
+++ android/src/main/java/com/reactlibrary/scene/nodes/views/MaxHeightScrollView.kt
@@ -1,3 +1,18 @@
+/*
+ *  Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.reactlibrary.scene.nodes.views
 
 import android.content.Context
@@ -8,12 +23,12 @@ import com.reactlibrary.utils.dp
 
 class MaxHeightScrollView(context: Context, attrs: AttributeSet) : ScrollView(context, attrs) {
 
-    private var maxHeight = 126.dp.toFloat()
+    private var maxHeight = context.resources.getDimension(R.dimen.default_scroll_dialog_max_height)
 
     init {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.MaxHeightScrollView, 0, 0)
         try {
-            maxHeight = typedArray.getDimension(R.styleable.MaxHeightScrollView_maxHeight, 126.dp.toFloat())
+            maxHeight = typedArray.getDimension(R.styleable.MaxHeightScrollView_maxHeight, maxHeight)
         } finally {
             typedArray.recycle()
         }
--- android/src/test/java/com/reactlibrary/scene/nodes/DialogNodeTest.kt
+++ android/src/test/java/com/reactlibrary/scene/nodes/DialogNodeTest.kt
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package com.reactlibrary.scene.nodes
 
 import android.content.Context
@@ -23,13 +39,17 @@ class DialogNodeTest {
     private lateinit var iconsRepository: IconsRepository
     private lateinit var testDialogProvider: DialogProvider
     private lateinit var context: Context
+    private lateinit var dummyDrawable: Drawable
+    private lateinit var mockDialogBuilder: CustomAlertDialogBuilder
 
     @Before
     fun setUp() {
+        dummyDrawable = mock()
+        mockDialogBuilder = mock()
         iconsRepository = mock()
         testDialogProvider = mock()
-
         context = ApplicationProvider.getApplicationContext()
+
     }
 
     @Test
@@ -39,16 +59,14 @@ class DialogNodeTest {
                 DialogNode.PROP_CONFIRM_TEXT, "text"
         )
         val node = DialogNode(props, context, iconsRepository, testDialogProvider)
-        val mockDialogBuilder: CustomAlertDialogBuilder = mock()
-        val testDrawable = mock<Drawable>()
         whenever(testDialogProvider.provideCustomAlertDialogBuilder(any())).thenReturn(mockDialogBuilder)
-        whenever(iconsRepository.getIcon("icon", false)).thenReturn(testDrawable)
+        whenever(iconsRepository.getIcon("icon", false)).thenReturn(dummyDrawable)
 
         node.build()
 
         verify(mockDialogBuilder).setConfirmationText("text")
         verify(iconsRepository).getIcon("icon", false)
-        verify(mockDialogBuilder).setConfirmationIcon(testDrawable)
+        verify(mockDialogBuilder).setConfirmationIcon(dummyDrawable)
     }
 
     @Test
@@ -58,16 +76,14 @@ class DialogNodeTest {
                 DialogNode.PROP_CANCEL_TEXT, "text"
         )
         val node = DialogNode(props, context, iconsRepository, testDialogProvider)
-        val mockDialogBuilder: CustomAlertDialogBuilder = mock()
-        val testDrawable = mock<Drawable>()
         whenever(testDialogProvider.provideCustomAlertDialogBuilder(any())).thenReturn(mockDialogBuilder)
-        whenever(iconsRepository.getIcon("icon", false)).thenReturn(testDrawable)
+        whenever(iconsRepository.getIcon("icon", false)).thenReturn(dummyDrawable)
 
         node.build()
 
         verify(mockDialogBuilder).setCancelText("text")
         verify(iconsRepository).getIcon("icon", false)
-        verify(mockDialogBuilder).setCancelIcon(testDrawable)
+        verify(mockDialogBuilder).setCancelIcon(dummyDrawable)
     }
 
     @Test
@@ -77,7 +93,6 @@ class DialogNodeTest {
                 DialogNode.PROP_TEXT, "text"
         )
         val node = DialogNode(props, context, iconsRepository, testDialogProvider)
-        val mockDialogBuilder: CustomAlertDialogBuilder = mock()
         whenever(testDialogProvider.provideCustomAlertDialogBuilder(any())).thenReturn(mockDialogBuilder)
 
         node.build()

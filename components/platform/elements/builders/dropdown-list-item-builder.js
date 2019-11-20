3f24683d2abaf6a02f57aa44c9bbbdc594003c3a
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-11-20 08:59:46 +0100

    #244 Add PanelBuilder.

--- components/platform/elements/builders/dropdown-list-item-builder.js
+++ components/platform/elements/builders/dropdown-list-item-builder.js
@@ -1,7 +1,7 @@
 import ElementBuilder from './element-builder';
 
 export default class DropdownListItemBuilder extends ElementBuilder {
-    create(props, id) {
-        this._componentManager.createDropdownListItemNode(props, id);
-    }
+  create(props, id) {
+    this._componentManager.createDropdownListItemNode(props, id);
+  }
 }
--- components/platform/elements/builders/text-edit-builder.js
+++ components/platform/elements/builders/text-edit-builder.js
@@ -1,7 +1,7 @@
 import ElementBuilder from './element-builder';
 
 export default class TextEditBuilder extends ElementBuilder {
-    create(props, id) {
-        this._componentManager.createTextEditNode(props, id);
-    }
+  create(props, id) {
+    this._componentManager.createTextEditNode(props, id);
+  }
 }

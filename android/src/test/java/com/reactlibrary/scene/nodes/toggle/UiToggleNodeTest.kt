91d4d84a6fab51a70282e3dcf987d45824d81f5d
Dariusz Konieczko <d.konieczko@nomtek.com>
2019-11-22 14:09:14 +0100

    refactor

--- android/src/test/java/com/reactlibrary/scene/nodes/toggle/UiToggleNodeTest.kt
+++ android/src/test/java/com/reactlibrary/scene/nodes/toggle/UiToggleNodeTest.kt
@@ -1,17 +1,17 @@
 /*
- * Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved
+ *  Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package com.reactlibrary.scene.nodes.toggle
@@ -24,8 +24,6 @@ import com.facebook.react.bridge.ReadableMap
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
-import com.reactlibrary.scene.nodes.toggle.ToggleViewManager
-import com.reactlibrary.scene.nodes.toggle.UiToggleNode
 import com.reactlibrary.utils.Utils
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -48,17 +46,6 @@ class UiToggleNodeTest {
         this.context = ApplicationProvider.getApplicationContext()
         this.toggleViewManager = mock()
     }
-
-    /*
-    @Test
-    fun `should use typeface from provider`() {
-        val node = createNode(JavaOnlyMap())
-
-        node.build()
-
-        verify(textViewSpy).typeface = providerTypeface
-    }
-     */
 
     @Test
     fun `should have default height`() {
@@ -113,41 +100,6 @@ class UiToggleNodeTest {
 
         verify(toggleViewManager).setTextColor(0xFFFFFFFF.toInt())
     }
-
-    /*
-    @Test
-    fun `should set switch icon when type not specified`() {
-        val node = createNode(JavaOnlyMap())
-
-        node.build()
-
-        verify(imageViewSpy).setImageResource(R.drawable.switch_off)
-    }
-
-    @Test
-    fun `should set checkbox icon when checkbox property present`() {
-        val props = JavaOnlyMap.of(UiToggleNode.PROP_TYPE, UiToggleNode.TYPE_CHECKBOX)
-        val node = createNode(props)
-
-        node.build()
-
-        verify(imageViewSpy).setImageResource(R.drawable.checkbox_off)
-    }
-
-    @Test
-    fun `should set checked radio icon when type is radio and checked`() {
-        val props = JavaOnlyMap.of(
-                UiToggleNode.PROP_TYPE, UiToggleNode.TYPE_RADIO,
-                UiToggleNode.PROP_CHECKED, true
-        )
-        val node = createNode(props)
-
-        node.build()
-
-        verify(imageViewSpy).setImageResource(R.drawable.radio_on)
-    }
-
-    */
 
     private fun createNode(props: ReadableMap): UiToggleNode {
         return UiToggleNode(props, context, mock(), toggleViewManager)

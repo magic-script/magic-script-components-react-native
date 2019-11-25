b0430d8122c81c680c505e082d2628fc6860cb5e
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2019-11-25 13:24:06 +0100

    Add missing code for showing debug view controller in HostApplication.

--- ios/RNMagicScriptHostApplication/sources/AppDelegate.swift
+++ ios/RNMagicScriptHostApplication/sources/AppDelegate.swift
@@ -21,6 +21,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
         return true
     }
 }

13fb68074ea861dc39902f9e3789add2460734eb
Dariusz Konieczko <d.konieczko@nomtek.com>
2019-11-27 14:33:29 +0100

    comment out

--- components/platform/platform-events.js
+++ components/platform/platform-events.js
@@ -8,6 +8,7 @@ export const Events = [
     name: "onClick",
     handler: (componentManager, elementId) => componentManager.addOnActivateEventHandler(elementId)
   },
+  /*
   {
     name: "onPress",
     handler: (componentManager, elementId) => componentManager.addOnPressEventHandler(elementId)
@@ -44,7 +45,7 @@ export const Events = [
     name: "onDelete",
     handler: (componentManager, elementId) => componentManager.addOnDeleteEventHandler(elementId)
   },
-
+*/
   // Other events
   {
     name: "onDialogConfirmed",
